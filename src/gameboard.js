cc3b44df32bdf268bb47cfa8925255f60dd405f9
Lintalou <148902267+Lintalou@users.noreply.github.com>
2025-11-04 16:44:07 +0700

    Added constructor for the class.

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,3 +1,17 @@
-class GameBoard { }
+function createEmptyBoard() {
+    const board = new Array(10);
+
+    for (let i = 0; i < board.length; i++) {
+        board[i] = new Array(10);
+    }
+
+    return board;
+}
+
+class GameBoard {
+    constructor() {
+        this.board = createEmptyBoard();
+    }
+}
 
 export { GameBoard }
