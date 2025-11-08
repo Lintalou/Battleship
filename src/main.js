1e31acbded527f407e2f215c462ab038b10da9cc
Lintalou <148902267+Lintalou@users.noreply.github.com>
2025-11-09 00:02:52 +0700

    File renamed.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import { Ship } from "./ship.js";
 import { Player } from "./player.js";
 
-const t = new Player("James");
-const j = new Ship(2)
-
-console.log(j);
+const player = new Player("player");
