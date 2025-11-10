19009655b47e19de8d79375ff7068c0c1ff58165
Lintalou <148902267+Lintalou@users.noreply.github.com>
2025-11-11 06:42:51 +0700

    Avoided filling because of high memory usage.

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,7 +2,7 @@ function createEmptyBoard() {
     const board = new Array(10);
 
     for (let i = 0; i < board.length; i++) {
-        board[i] = new Array(10).fill("");
+        board[i] = new Array(10);
     }
 
     return board;
