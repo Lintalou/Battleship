520235de92e577c7e81b083020fee50f9c4fc4c4
Lintalou <148902267+Lintalou@users.noreply.github.com>
2025-11-11 06:31:11 +0700

    Added a WIP start function.

--- src/DOM.js
+++ src/DOM.js
@@ -17,3 +17,18 @@ function displayPrimaryBoard() {
         })
     })
 }
+
+function start() {
+    generatePlayer("player");
+    generatePlayer("computer");
+
+    const patrolBoat = generateShip(2);
+    const carrier = generateShip(5);
+
+    players[0].gameBoard.place(patrolBoat, [6, "F"], "vertical");
+    players[0].gameBoard.place(carrier, [10, "A"], "horizontal");
+
+    displayPrimaryBoard();
+}
+
+startButton.addEventListener("click", start);
