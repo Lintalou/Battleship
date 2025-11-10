b373ca3613782338e54bca5b092877a6de053d0d
Lintalou <148902267+Lintalou@users.noreply.github.com>
2025-11-11 04:48:47 +0700

    Added an object to store players' data and modified the function to generate player to work with it.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import { Ship } from "./ship.js";
 import { Player } from "./player.js";
 
+const data = {
+    player: []
+}
+
 function generatePlayer(name) {
-    return new Player(name);
+    data.player.push(new Player(name));
 }
 
 function generateShip(size) {
     return new Ship(size);
 }
 
-export { generatePlayer, generateShip }
+export { generatePlayer, generateShip, data }
