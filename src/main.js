0611f0da79299144aa6659f8073dc73378f50156
Lintalou <148902267+Lintalou@users.noreply.github.com>
2025-11-11 05:13:52 +0700

    Changed the object to an array.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 import { Ship } from "./ship.js";
 import { Player } from "./player.js";
 
-const data = {
-    player: []
-}
+const players = [];
 
 function generatePlayer(name) {
-    data.player.push(new Player(name));
+    players.push(new Player(name));
 }
 
 function generateShip(size) {
     return new Ship(size);
 }
 
-export { generatePlayer, generateShip, data }
+export { generatePlayer, generateShip, players }
