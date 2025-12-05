f625e5c26ee182b37e535b9a435eb521e8171aaf
Lintalou <148902267+Lintalou@users.noreply.github.com>
2025-12-05 20:02:38 +0700

    Added a file for checking placement of the ships with some WIP codes.

---
+++ src/game/checkPlacement.js
@@ -0,0 +1,64 @@
+function checkHorizontal(board, ship, coord) {
+    for (let i = 0; i < ship.length; i++) {
+        if (coord[1] + i > 9) {
+            return false;
+        }
+
+        if (board[coord[0]][coord[1] + i]) {
+            return false;
+        }
+
+        if (i === 0) {
+            if (board[coord[0]][coord[1] - 1]) {
+                return false;
+            }
+
+            if (board[coord[0] - 1][coord[1]]) {
+                return false;
+            }
+
+            if (board[coord[0] + 1][coord[1]]) {
+                return false;
+            }
+        } else if (i === ship.length - 1) {
+            if (board[coord[0]][coord[1] + 1]) {
+                return false;
+            }
+
+            if (board[coord[0] - 1][coord[1]]) {
+                return false;
+            }
+
+            if (board[coord[0] + 1][coord[1]]) {
+                return false;
+            }
+        } else {
+            if (board[coord[0] - 1][coord[1]]) {
+                return false;
+            }
+
+            if (board[coord[0] + 1][coord[1]]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+function checkVertical(board, ship, coord) {
+    for (let i = 0; i < ship.length; i++) {
+        if (coord[0] + i > 9) {
+            return false;
+        }
+
+        if (board[coord[0] + i][coord[1]]) {
+            return false;
+        }
+    }
+
+}
+
+function checkPlacement(board, ship, coord) {
+    const coordNum = [coord[0] - 1, changeLetterToNum(coord[1])];
+}
